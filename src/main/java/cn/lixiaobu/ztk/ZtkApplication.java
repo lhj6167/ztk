fb1045918fd57066b4a54175de617c58a6e6a5f4
Administrator <970464866@qq.com>
2023-08-29 21:24:50 +0800

    Initial commit

---
+++ src/main/java/cn/lixiaobu/ztk/ZtkApplication.java
@@ -0,0 +1,13 @@
+package cn.lixiaobu.ztk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZtkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/lixiaobu/ztk/ZtkApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lixiaobu.ztk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZtkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
